f88ee61f4aa18f7c8e17bacdb64e0f0f56469eae
Holtkamp, Jason <jholtkamp@conversantmedia.com>
2017-01-26 14:42:42 -0600

    equals button now accepts all necessary args

--- client/components/CalculateButtonComponent.jsx
+++ client/components/CalculateButtonComponent.jsx
@@ -3,16 +3,22 @@ import React, { Component, PropTypes } from 'react';
 class CalculateButtonComponent extends Component {
   constructor(props){
     super(props);
+    this.onEqualsButtonClick = this.onEqualsButtonClick.bind(this);
   }
 
   componentDidMount() {
   
   }
 
+  onEqualsButtonClick(num1, operation, num2){
+    console.log("equals buttons was clicked");
+    console.log(num1, operation, num2);
+  }
+
   render() {
 
     return (
-      <button>=</button>
+      <button onClick={() => {this.onEqualsButtonClick(this.props.previousNumber, this.props.operation, this.props.currentNumber)}}>=</button>
     )
   }
 }
--- client/containers/CalculateButtonContainer.js
+++ client/containers/CalculateButtonContainer.js
@@ -7,7 +7,9 @@ const mapDispatchToProps = dispatch => ({
 });
 
 const mapStateToProps = state => ({
-
+	currentNumber: state.numbers.currentNumber,
+	previousNumber: state.numbers.previousNumber,
+	operation: state.currentOperation,
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(CalculateButtonComponent);
