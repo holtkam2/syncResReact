bffab9c445cec1dc7ee7bb69aa57e2fc69e1f4d6
Holtkamp, Jason <jholtkamp@conversantmedia.com>
2017-01-31 10:11:06 -0600

    last commit before making a new branch

--- client/components/DisplayComponent.jsx
+++ client/components/DisplayComponent.jsx
@@ -1,17 +1,27 @@
 import React, { Component, PropTypes } from 'react';
 import Paper from 'material-ui/Paper';
 
-class DisplayComponent extends Component {
-  constructor(props){
-    super(props);
-  }
+	class DisplayComponent extends Component {
+  	constructor(props){
+    	super(props);
+  	}
 
   render() {
+
+	  const style = {
+	  	width: 200,
+		  margin: 20,
+		  textAlign: 'center',
+		  display: 'inline-block',
+		};
+
     return (
-      <div className="DisplayComponent">{this.props.currentNumber}</div>
+    	<Paper style={style} zDepth={3}>
+      	<div className="DisplayComponent">{this.props.currentNumber}</div>
+      </Paper>
     )
-  
   }
+
 }
 
 export default DisplayComponent;
