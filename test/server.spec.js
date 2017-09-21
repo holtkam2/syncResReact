4edcbc300b26e0b2dd9afc94d0e7c884bce2eceb
Holtkamp, Jason <jholtkamp@conversantmedia.com>
2017-09-21 15:54:09 -0500

    added first test for server

--- test/server.spec.js
+++ test/server.spec.js
@@ -1,1 +1,7 @@
-console.log("hello!");
+var expect = require('chai').expect;
+
+describe('dummy test', function() {
+    it('should test for something', function(){
+        expect(1).to.equal(1);
+    })
+});
